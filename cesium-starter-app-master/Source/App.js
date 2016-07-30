b6db5192fdb99b30c1767bc43ebdd4ada1b3ba72
Miyu Hyakuda <machimachi.bbc.1020@gmail.com>
2016-07-31 02:43:10 +0900

    add

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,11 +1,28 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
-//add a point
-var point = viewer.entities;
 
-point.add({
-	position : Cesium.Cartesian3.fromDegrees(-75.59777,40.03883),
-	point : {
-		pixelSize : 10,
-		color : Cesium.Color.YELLOW
- }
+
+var greenCylinder = viewer.entities.add({
+    name : 'Green cylinder with black outline',
+    position: Cesium.Cartesian3.fromDegrees(-100.0, 40.0, 200000.0),
+    cylinder : {
+        length : 400000.0,
+        topRadius : 200000.0,
+        bottomRadius : 200000.0,
+        material : Cesium.Color.GREEN.withAlpha(0.5),
+        outline : true,
+        outlineColor : Cesium.Color.DARK_GREEN
+    }
 });
+
+var redCone = viewer.entities.add({
+    name : 'Red cone',
+    position: Cesium.Cartesian3.fromDegrees(-105.0, 40.0, 200000.0),
+    cylinder : {
+        length : 400000.0,
+        topRadius : 0.0,
+        bottomRadius : 200000.0,
+        material : Cesium.Color.RED
+    }
+});
+
+viewer.zoomTo(viewer.entities);
