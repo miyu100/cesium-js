f51e90f5dd744e103632136dd586b1743e46b7cf
Miyu Hyakuda <machimachi.bbc.1020@gmail.com>
2016-08-01 04:02:33 +0900

    add

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,6 +1,51 @@
-var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+ var viewer = new Cesium.Viewer('cesiumContainer');
+
+ viewer.camera.flyTo({
+      destination: Cesium.Cartesian3.fromDegrees(139.4130,35.4122,5000000)
+    });
 
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('https://github.com/miyu100/cesium-js/blob/gh-pages/cesium-starter-app-master/Source/Australia.kml'));
+var hcc0 = viewer.entities.add({
+  position: Cesium.Cartesian3.fromDegrees( 145.451071,44.333008,0.0),
+  name:'hcc0',
+  ellipse:{
+    semiMinorAxis : 50000.0,
+    semiMajorAxis : 50000.0,
+    height : 10.0,
+    material : Cesium.Color.GREEN.withAlpha(0.5),
+  }
+});
 
-viewer.zoomTo(viewer.entities);
+var hcc1 = viewer.entities.add({
+  position: Cesium.Cartesian3.fromDegrees( 140.451071,40.333008,0.0),
+  name:'hcc1',
+  ellipse:{
+    semiMinorAxis : 50000.0,
+    semiMajorAxis : 50000.0,
+    height : 10.0,
+    material : Cesium.Color.GREEN.withAlpha(0.5),
+  }
+});
+
+var hcc2 = viewer.entities.add({
+  position: Cesium.Cartesian3.fromDegrees( 130.451071,30.333008,0.0),
+  name:'hcc2',
+  ellipse:{
+    semiMinorAxis : 50000.0,
+    semiMajorAxis : 50000.0,
+    height : 10.0,
+    material : Cesium.Color.GREEN.withAlpha(0.5),
+  }
+});
+
+var hcc3 = viewer.entities.add({
+  position: Cesium.Cartesian3.fromDegrees( 142.451071,27.333008,0.0),
+  name:'hcc3',
+  ellipse:{
+    semiMinorAxis : 50000.0,
+    semiMajorAxis : 50000.0,
+    height : 10.0,
+    material : Cesium.Color.GREEN.withAlpha(0.5),
+  }
+});
+
