b221689cd44b9126180dfe9b6713caa41766ca56
Miyu Hyakuda <machimachi.bbc.1020@gmail.com>
2016-07-31 14:36:28 +0900

    add

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,6 +1,6 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
 
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('https://github.com/miyu100/cesium-js/blob/gh-pages/cesium-starter-app-master/Source/Australia.kml')
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://github.com/miyu100/cesium-js/blob/gh-pages/cesium-starter-app-master/Source/Australia.kml'));
 
 viewer.zoomTo(viewer.entities);
